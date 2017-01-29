58ef0c32338a0e33313c1ab4b9b167a6dbc8c638
Aleksey Samusenko <aleksej.samusenko93@gmail.com>
2017-01-29 19:27:55 +0200

    function 'setNotification' is done

--- js/idashboard/components/ClipBoard_TD.js
+++ js/idashboard/components/ClipBoard_TD.js
@@ -2,7 +2,9 @@
 		if(this.getActiveTab() != "remoteaccess"){
 			return
 		}
+		this.tunnelDetails = document.querySelectorAll('td.tunnel_details');
 		this.setAutoClipBoard();
+		this.setNotification();
 	};
 
 	ClipBoard_TD.prototype = Object.create(App.prototype);
@@ -10,12 +12,10 @@
 
 	ClipBoard_TD.prototype.setAutoClipBoard = function(){
 
-		var tunnelDetails = document.querySelectorAll('td.tunnel_details');
+		for(var i = 0, arrLength = this.tunnelDetails.length ; i < arrLength ; i++ ){
 
-		for(var i = 0, arrLength = tunnelDetails.length ; i < arrLength ; i++ ){
-
-			var currentTunnelDetails = tunnelDetails[i].innerText,
-                currentTunnelDetailsHtml = tunnelDetails[i];
+			var currentTunnelDetails = this.tunnelDetails[i].innerText,
+                currentTunnelDetailsHtml = this.tunnelDetails[i];
 
             if(currentTunnelDetails.slice(0, 3) == "ssh"){
             	currentTunnelDetailsHtml.className += " btn";
@@ -23,7 +23,21 @@
                 currentTunnelDetailsHtml.setAttribute("data-clipboard-target", "#id" + i);
             };    
 		};
-	}
+	};
+
+	ClipBoard_TD.prototype.setNotification = function() {
+
+		var tableContainer = document.querySelector('.table-container');
+
+			tableContainer.addEventListener('click', this.showNotification, false);
+
+		
+
+	};
+
+
+
+
 
 	new App();
 
